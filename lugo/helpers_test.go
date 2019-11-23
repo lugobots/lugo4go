0062c26bf877ac2676110c361d1e78fe88ffefb4
Rubens Silva <rubens21@gmail.com>
2019-11-23 17:49:15 -0500

    testing for GetPlay

--- lugo/helpers_test.go
+++ lugo/helpers_test.go
@@ -6,30 +6,27 @@ func TestGetTeam_GetsTheRightTeam(t *testing.T) {
 	expected := Team{Name: "MAY TEAM"}
 	snapshot := GameSnapshot{HomeTeam: &expected}
 
-	got := GetTeam(&snapshot, Team_HOME)
-	if got != &expected {
-		t.Errorf("Unexpedted Team - Expected %v, got %v", expected, got)
+	if got := GetTeam(&snapshot, Team_HOME); got != &expected {
+		t.Errorf("Unexpected Team - Expected %v, got %v", expected, got)
 	}
+
 	expected = Team{Name: "Another Team"}
 	snapshot = GameSnapshot{AwayTeam: &expected}
 
-	got = GetTeam(&snapshot, Team_AWAY)
-	if got != &expected {
-		t.Errorf("Unexpedted Team - Expected %v, got %v", expected, got)
+	if got := GetTeam(&snapshot, Team_AWAY); got != &expected {
+		t.Errorf("Unexpected Team - Expected %v, got %v", expected, got)
 	}
 }
 
 func TestGetTeam_DoNotPanicInvalidSnapshot(t *testing.T) {
 	snapshot := GameSnapshot{}
 
-	got := GetTeam(&snapshot, Team_HOME)
-	if got != nil {
-		t.Errorf("Unexpedted value - Expected nil, got %v", got)
+	if got := GetTeam(&snapshot, Team_HOME); got != nil {
+		t.Errorf("Unexpected value - Expected nil, got %v", got)
 	}
 
-	got = GetTeam(nil, Team_HOME)
-	if got != nil {
-		t.Errorf("Unexpedted value - Expected nil, got %v", got)
+	if got := GetTeam(nil, Team_HOME); got != nil {
+		t.Errorf("Unexpected value - Expected nil, got %v", got)
 	}
 }
 
@@ -39,7 +36,7 @@ func TestIsBallHolder_ShouldBeTrue(t *testing.T) {
 	snapshot := GameSnapshot{Ball: &ball}
 
 	if !IsBallHolder(&snapshot, &expectedHolder) {
-		t.Errorf("Unexpedted value - Expected true, got false")
+		t.Errorf("Unexpected value - Expected true, got false")
 	}
 }
 
@@ -49,7 +46,7 @@ func TestIsBallHolder_ShouldBeFalse_NoHolder(t *testing.T) {
 	snapshot := GameSnapshot{Ball: &ball}
 
 	if IsBallHolder(&snapshot, &expectedHolder) {
-		t.Errorf("Unexpedted value - Expected false, got true")
+		t.Errorf("Unexpected value - Expected false, got true")
 	}
 }
 
@@ -59,7 +56,7 @@ func TestIsBallHolder_ShouldBeFalse_OtherPlayerHolds(t *testing.T) {
 	snapshot := GameSnapshot{Ball: &ball}
 
 	if IsBallHolder(&snapshot, &expectedHolder) {
-		t.Errorf("Unexpedted value - Expected false, got true")
+		t.Errorf("Unexpected value - Expected false, got true")
 	}
 }
 
@@ -68,13 +65,56 @@ func TestIsBallHolder_ShouldBeFalse_InvalidInputs(t *testing.T) {
 	snapshot := GameSnapshot{}
 
 	if IsBallHolder(&snapshot, &expectedHolder) {
-		t.Errorf("Unexpedted value - Expected false, got true")
+		t.Errorf("Unexpected value - Expected false, got true")
 	}
 
 	if IsBallHolder(nil, &expectedHolder) {
-		t.Errorf("Unexpedted value - Expected false, got true")
+		t.Errorf("Unexpected value - Expected false, got true")
 	}
 	if IsBallHolder(&GameSnapshot{Ball: &Ball{Holder: &expectedHolder}}, nil) {
-		t.Errorf("Unexpedted value - Expected false, got true")
+		t.Errorf("Unexpected value - Expected false, got true")
+	}
+}
+
+func TestGetPlayer(t *testing.T) {
+	expectedPlayer := Player{TeamSide: Team_HOME, Number: 11}
+	snapshot := GameSnapshot{
+		HomeTeam: &Team{Players: []*Player{
+			&expectedPlayer,
+		}},
+	}
+
+	if got := GetPlayer(&snapshot, Team_HOME, 11); got != &expectedPlayer {
+		t.Errorf("Unexpected value - Expected %v, got %v", expectedPlayer, got)
+	}
+}
+
+func TestGetPlayer_PlayerNotPresent(t *testing.T) {
+	snapshot := GameSnapshot{
+		HomeTeam: &Team{Players: []*Player{
+			{TeamSide: Team_HOME, Number: 10},
+		}},
+	}
+
+	if got := GetPlayer(&snapshot, Team_HOME, 11); got != nil {
+		t.Errorf("Unexpected value - Expected nil, got %v", got)
+	}
+}
+
+func TestGetPlayer_TeamNotPresent(t *testing.T) {
+	snapshot := GameSnapshot{
+		HomeTeam: &Team{Players: []*Player{
+			{TeamSide: Team_HOME, Number: 10},
+		}},
+	}
+
+	if got := GetPlayer(&snapshot, Team_AWAY, 10); got != nil {
+		t.Errorf("Unexpected value - Expected nil, got %v", got)
+	}
+}
+
+func TestGetPlayer_InvalidSnapshot(t *testing.T) {
+	if got := GetPlayer(nil, Team_AWAY, 11); got != nil {
+		t.Errorf("Unexpected value - Expected nil, got %v", got)
 	}
 }
